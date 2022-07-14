cc049563c28b194251c5e66e7e16292c356952d0
mmulika <mulikamike@gmail.com>
2022-07-14 16:36:37 +0300

    more classes

---
+++ employee.py
@@ -0,0 +1,17 @@
+class Employee():
+    def __init__(self, name, age,income):
+     self.name = name
+     self.age = age
+     self.income =income
+    def  details(self):
+        print(self.name)
+        print(self.age)
+        print(self.income)
+    
+    def benefits(self):
+        commission =(self.income * 10)/100
+        return (commission)
+employee1 =Employee('boss',10,1000)
+
+print(employee1.details())
+print(employee1.benefits())
