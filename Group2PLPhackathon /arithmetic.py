fe9528edb811d965be108974b29818842f0adc03
mmulika <mulikamike@gmail.com>
2022-07-19 11:06:00 +0300

    ifelse

---
+++ Group2PLPhackathon /arithmetic.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    a = int(input("enter number:"))
+    b = int(input("enter number:"))
+    add = (a + b)
+    sub = (a - b)
+    mult = (a * b )
+    print(add,sub,mult)
